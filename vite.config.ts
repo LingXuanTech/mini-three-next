8c23c53508c04358a34236cfd824698db9294178
qiandu <qiandu@qunhemail.com>
2025-01-17 15:56:17 +0800

    feat: :sparkles: init repo

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+import { defineConfig, UserConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+    base: "./",
+    plugins: [dts({ rollupTypes: true })],
+    build: {
+        outDir: "dist",
+        sourcemap: true,
+        lib: {
+            entry: path.resolve(__dirname, "./src/index.ts"),
+            name: "mini-three-next",
+            formats: ["es", "cjs", "umd", "iife"],
+            fileName: (format) => `index.${format}.js`,
+        },
+    },
+} satisfies UserConfig);
